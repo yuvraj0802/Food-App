20936db6a086733c855ae180353c7d0c0e4fe6c8
yuvraj0802 <105724665+yuvraj0802@users.noreply.github.com>
2024-02-04 13:13:54 -0500

    make
    change

---
+++ Backend/secrets.js
@@ -0,0 +1,13 @@
+
+
+module.exports = {
+    db_link:"mongodb+srv://Yuvraj:GJ5kHNFT5uXlZrAj@cluster0.zek8tmn.mongodb.net/?retryWrites=true&w=majority",
+    JWT_KEY:"rbfywg8374bfuiwebf"
+}
+
+//mongodb 
+//stripe
+//nodemailer
+
+
+    
